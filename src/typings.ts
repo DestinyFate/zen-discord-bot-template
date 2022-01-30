80a4219c3b002bee0a60c3a02a70a3f06547ab1a
Elitezen <61894124+Elitezen@users.noreply.github.com>
2022-01-29 21:49:16 -0500

    Removed Redis Client from Typings

--- src/typings.ts
+++ src/typings.ts
@@ -7,11 +7,11 @@ import {
 
 export interface SlashCommand {
   data: ApplicationCommandData;
-  execute(DiscordClient:Client, RedisClient:object, interaction:CommandInteraction): void;
+  execute(DiscordClient:Client, interaction:CommandInteraction): void;
 }
 
 export interface DiscordEvent {
   name: keyof ClientEvents;
   once: boolean;
-  execute(DiscordClient:Client, RedisClient:object, ...args:unknown[]): void;
+  execute(DiscordClient:Client, ...args:unknown[]): void;
 }
